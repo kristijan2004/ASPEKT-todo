06e3f90c7460c387bb3bdb5f6ecf8cc757bee1cd
Kiko <kristijanivanovski2004@gmail.com>
2023-02-04 18:09:56 +0100

    added sorting functions, edit and delete buttons

--- src/components/Context.js
+++ src/components/Context.js
@@ -6,16 +6,46 @@ export const Provider = (props) => {
   const [inputValue, setInputValue] = useState('');
   const [todoList, setTodoList] = useState([]);
   const [deletedToDo, setDeletedToDo] = useState({});
+  const [editToDo, setEditToDo] = useState({});
+  const [editMode, setEditMode] = useState(false);
 
-  const AddToDo = (inputValue) => {
-    let date = new Date();
-    let toDo = {
-      name: inputValue,
-      status: 'active',
-      date: date,
-      id: todoList.length === 0 ? 0 : todoList.length
-    };
-    setTodoList([...todoList, toDo]);
+  const AddToDo = (inputValue, date, id) => {
+    let sameName = todoList.find((el) => el.name === inputValue);
+    if (sameName) {
+      alert('error');
+    } else {
+      if (editMode) {
+        let toDo = {
+          name: inputValue,
+          status: 'active',
+          date: date,
+          id: id
+        };
+        let temporaryArr = [...todoList];
+        temporaryArr.splice(id, 1, toDo);
+        setTodoList(temporaryArr);
+        document.getElementById(id).style.textDecoration = 'none';
+        setEditMode(false);
+      } else {
+        let newDate = new Date();
+        let formatedDate = newDate.toISOString().substr(0, 10);
+        // let day = newDate.getDate();
+        // let month = newDate.getMonth();
+        // let year = newDate.getFullYear();
+
+        let toDo = {
+          name: inputValue,
+          status: 'active',
+          // date: `${day.length > 1 ? day : '0' + day}.${
+          //   month.length > 1 ? month + 1 : '0' + (month + 1)
+          // }.${year}`,
+          date: formatedDate,
+          id: todoList.length === 0 ? 0 : todoList.length
+        };
+        setTodoList([...todoList, toDo]);
+      }
+    }
+    document.getElementById('input').value = '';
   };
 
   const deleteToDo = (id) => {
@@ -26,6 +56,115 @@ export const Provider = (props) => {
     Array.map((el) => (el.id = Array.map((ele) => ele.name).indexOf(el.name)));
     setTodoList(Array);
   };
+  const EditToDo = (id) => {
+    let elementToBeEdited = todoList.find((el) => el.id === id);
+    setEditToDo(elementToBeEdited);
+    document.getElementById('input').value = elementToBeEdited.name;
+    document.getElementById(id).style.textDecoration = 'line-through';
+    setEditMode(true);
+  };
+  const FinishToDo = (id) => {
+    let finishedToDo = todoList.find((el) => el.id === id);
+    finishedToDo.status = 'completed';
+    let temporaryArr = [...todoList];
+    temporaryArr.splice(id, 1, finishedToDo);
+    setTodoList(temporaryArr);
+    document.getElementById(id).style.textDecoration = 'line-through';
+    let btns =
+      document.getElementById(id).nextElementSibling.nextElementSibling
+        .nextElementSibling.childNodes;
+    btns.forEach((el) => el.setAttribute('disabled', 'true'));
+  };
+  const SortByNameAscending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+  };
+  const SortByNameDescending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      }
+      return 0;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+  };
+  const SortByDateAscending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      let aDate = new Date(a.date);
+      let bDate = new Date(b.date);
+      return aDate - bDate;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+    console.log('asd');
+  };
+  const SortByDateDescending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      let aDate = new Date(a.date);
+      let bDate = new Date(b.date);
+      return bDate - aDate;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+    console.log('asd');
+  };
+  const SortByStatusAscending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      if (a.status < b.status) {
+        return -1;
+      }
+      if (a.status > b.status) {
+        return 1;
+      }
+      return 0;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+  };
+  const SortByStatusDescending = (array) => {
+    let arrayToBeSorted = [...array];
+    arrayToBeSorted.sort((a, b) => {
+      if (a.status < b.status) {
+        return 1;
+      }
+      if (a.status > b.status) {
+        return -1;
+      }
+      return 0;
+    });
+    arrayToBeSorted.map(
+      (el) => (el.id = arrayToBeSorted.map((ele) => ele.name).indexOf(el.name))
+    );
+    setTodoList(arrayToBeSorted);
+  };
   return (
     <Context.Provider
       value={{
@@ -34,7 +173,17 @@ export const Provider = (props) => {
         AddToDo,
         inputValue,
         setInputValue,
-        deleteToDo
+        deleteToDo,
+        EditToDo,
+        editMode,
+        editToDo,
+        FinishToDo,
+        SortByNameAscending,
+        SortByNameDescending,
+        SortByDateAscending,
+        SortByDateDescending,
+        SortByStatusAscending,
+        SortByStatusDescending
       }}
     >
       {props.children}
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,27 +1,98 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from './Context';
 
 const InputForm = () => {
-  const { AddToDo, inputValue, setInputValue, todoList, deleteToDo } =
-    useContext(Context);
+  const [asceDesceStatus, setAsceDesceStatus] = useState(true);
+  const [asceDesceDate, setAsceDesceDate] = useState(true);
+  const [asceDesceName, setAsceDesceName] = useState(true);
+  const {
+    AddToDo,
+    inputValue,
+    setInputValue,
+    todoList,
+    deleteToDo,
+    EditToDo,
+    editMode,
+    editToDo,
+    FinishToDo,
+    SortByNameAscending,
+    SortByNameDescending,
+    SortByDateAscending,
+    SortByDateDescending,
+    SortByStatusAscending,
+    SortByStatusDescending
+  } = useContext(Context);
   return (
     <>
       <form
+        autoComplete='off'
         onSubmit={(e) => {
           e.preventDefault();
-          AddToDo(inputValue);
-          console.log(todoList);
+          {
+            editMode
+              ? AddToDo(inputValue, editToDo.date, editToDo.id)
+              : AddToDo(inputValue);
+          }
         }}
       >
-        <input type='text' onChange={(e) => setInputValue(e.target.value)} />
+        <input
+          type='text'
+          id='input'
+          onChange={(e) => setInputValue(e.target.value)}
+        />
         <input type='submit' />
       </form>
-      {todoList.map((el) => (
-        <div>
-          <span>{el.name}</span>
-          <button onClick={() => deleteToDo(el.id)}>delete {el.id}</button>
-        </div>
-      ))}
+      {editMode ? 'TRUE' : 'FALSE'}
+      <table>
+        <thead>
+          <tr>
+            <th
+              onClick={() =>
+                asceDesceName
+                  ? (SortByNameAscending(todoList), setAsceDesceName(false))
+                  : (SortByNameDescending(todoList), setAsceDesceName(true))
+              }
+            >
+              Name
+            </th>
+            <th
+              onClick={() =>
+                asceDesceDate
+                  ? (SortByDateAscending(todoList), setAsceDesceDate(false))
+                  : (SortByDateDescending(todoList), setAsceDesceDate(true))
+              }
+            >
+              Date
+            </th>
+            <th
+              onClick={() =>
+                asceDesceStatus
+                  ? (SortByStatusAscending(todoList), setAsceDesceStatus(false))
+                  : (SortByStatusDescending(todoList), setAsceDesceStatus(true))
+              }
+            >
+              Status
+            </th>
+            <th>Controls</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoList.map((el) => (
+            <tr key={el.id}>
+              <td id={el.id}>{el.name}</td>
+              <td>{el.date}</td>
+              <td>{el.status}</td>
+              <td>
+                <button onClick={() => deleteToDo(el.id)}>
+                  delete {el.id}
+                </button>
+                <button onClick={() => EditToDo(el.id)}>Edit</button>
+                <button onClick={() => FinishToDo(el.id)}>finish</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
